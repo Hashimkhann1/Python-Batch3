733cc32f902a35d67dcf7b7799432a140c569a94
Hashimkhann1 <84334197+Hashimkhann1@users.noreply.github.com>
2021-05-25 17:13:45 +0500

    lab 3 question 2 nested loops

---
+++ Qs2.py
@@ -0,0 +1,10 @@
+flong = int(input("how long is your flag"))
+fwide = int(input("how wide is your flage"))
+plong = int(input("how long is your flagpole"))
+pwide = int(input("how wide is your flagpole"))
+
+for a in range(flong):
+    print("*"*fwide)
+
+for b in range(plong):
+    print("*"*pwide)
