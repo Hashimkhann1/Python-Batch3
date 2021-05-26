c9129784dedad1e7f0b62b62b0bf692199b8b144
Hashimkhann1 <84334197+Hashimkhann1@users.noreply.github.com>
2021-05-26 21:13:09 +0500

    lab 4 question 2 part 1 and 2

---
+++ lab4q2p1.py
@@ -0,0 +1,10 @@
+print("enter the boolen values for X-OR gate")
+a = int(input("enter your first value"))
+b = int(input("enter your second value"))
+print("your first value X-OR gate",a,"\nyour second value for X-OR gate",b)
+if a==0 and b==0:
+    print("False")
+elif a==1 and b==1:
+    print("False")
+else:
+    print("True")
